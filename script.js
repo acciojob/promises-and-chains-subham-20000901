0c81411e1bac30201ed4b02a15a0a973e9f2de70
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-25 13:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+let ageInput = document.getElementById("age");
+let nameInput = document.getElementById("name");
+let button = document.getElementById("btn");
+
+ button.addEventListener('click', function(event) {
+
+	 event.preventDefault();
+	 
+	 const ageValue = ageInput.value;
+	 const name = nameInput.value;
+
+	 if( age === "" || name === "") {
+		 alert("Please enter valid details");
+		 return;
+	 }
+	 const age = Number(ageValue);
+
+	 const promiseResolution = new Promise((resolve,reject) => {
+	
+		if(age>18) {
+			
+			setTimeout(() => {
+				
+				resolve(`Welcome, ${name}. You can vote`);
+				
+			},4000);
+		}
+		else{
+			
+			setTimeout(() => {
+				reject(`Oh sorry ${name}. You aren't old enough`);
+			},4000);
+		}
+	});
+
+   
+	 promiseResolution
+		 .then(result => {
+		   alert(result);
+	 })
+	   .catch(error => {
+		   alert(error);
+	   });
+		 
+ });
+
+
+
